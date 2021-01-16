1bc5e2c15932ca5d51ba3d69e6dba133cea9ca44
namratha-98 <74010135+namratha-98@users.noreply.github.com>
2021-01-16 22:51:04 +0530

    Add files via upload

---
+++ heap.h
@@ -0,0 +1,21 @@
+#ifndef HEAP_H_INCLUDED
+#define HEAP_H_INCLUDED
+#include <stdint.h>
+#include "appconst.h"
+
+typedef struct heap Heap;
+
+struct heap {
+    uint32_t size;
+    int32_t  data[HEAP_MAX_SIZE];
+};
+
+Heap      heap_new(int32_t data[], uint32_t len); //building max heap
+Heap*     heap_sort(Heap *heap);
+Heap*     heap_insert(Heap *heap, int32_t key);
+Heap*     heap_test( Heap *heap);
+int32_t   heap_get_max(Heap *heap);
+int32_t   heap_extract_max(Heap *heap);
+uint32_t  heap_size(Heap *heap);
+
+#endif // HEAP_H_INCLUDED
